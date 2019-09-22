ef2d2280e6c52de39972777ad5fd77157f5c2955
Alex Ko <alexkosj@gmail.com>
2019-09-22 07:48:25 -0700

    create new file intended for new data source: aws cloud map namespaces

---
+++ aws/data_source_aws_private_dns_namespace.go
@@ -0,0 +1,10 @@
+package aws
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/servicediscovery"
+	"github.com/hashicorp/terraform/helper/schema"
+)
