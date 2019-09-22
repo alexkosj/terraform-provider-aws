e2cb7bac35423e1a341217b46d85c07cdeec3556
Alex Ko <alexkosj@gmail.com>
2019-09-22 08:28:20 -0700

    initial stubbing of data source definition and its read function

--- aws/data_source_aws_private_dns_namespace.go
+++ aws/data_source_aws_private_dns_namespace.go
@@ -8,3 +8,38 @@ import (
 	"github.com/aws/aws-sdk-go/service/servicediscovery"
 	"github.com/hashicorp/terraform/helper/schema"
 )
+
+func dataSourceAwsPrivateDnsNamespace() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceAwsPrivateDnsNamespaceRead,
+
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"arn": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"filter": sdCustomFiltersSchema(),
+
+			"tags": tagsSchemaComputed(),
+		},
+	}
+}
+
+func dataSourceAwsPrivateDnsNamespaceRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).sdconn
+
+	req := &servicediscovery.ListNamespaces{}
+}
